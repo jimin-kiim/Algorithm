02a1eea5358ed36730cf6b8a3bcf07e7b1049cfc
Jimin Kim <0305jimin@gmail.com>
2023-04-28 17:07:36 +0900

    Feat: annotating merge sort

--- C++/Algorithms/merge_sort.cpp
+++ C++/Algorithms/merge_sort.cpp
@@ -2,25 +2,41 @@
 // Created by 김지민 on 2023/04/28.
 //
 
+/*
+ * merge sort
+ *
+ * Design Approach: Divide & Conquer
+ *  Step 1. Divide the given array into more than 2 sub arrays
+ *  Step 2. Conquer(Solve) each subarray
+ *  Step 3. Combine the sub solutions
+ */
 #include <iostream>
 using namespace std;
 
+// merge two subarrays of array[]
+//
 void merge(int array[], int const left, int const mid, int const right) {
+
+    // size of the subarrays
     auto const subArrayOne = mid - left + 1;
     auto const subArrayTwo = right - mid;
 
+    // temp arrays
     auto *leftArray = new int[subArrayOne],
     *rightArray = new int[subArrayTwo];
 
+    // copying arrays to temp arrays
     for (auto i = 0; i < subArrayOne; i++)
         leftArray[i] = array[left + i];
     for (auto j = 0; j < subArrayTwo; j++)
         rightArray[j] = array[mid + 1 + j];
 
+    // intial index of subarrays and merged array
     auto indexOfSubArrayOne = 0,
     indexOfSubArrayTwo = 0;
     int indexOfMergedArray = left;
 
+    // merge the temp arrays back into array
     while(indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo){
         if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
             array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
@@ -32,13 +48,14 @@ void merge(int array[], int const left, int const mid, int const right) {
         indexOfMergedArray++;
     }
 
-
+    // copying the remaining elements of left[]
     while (indexOfSubArrayOne < subArrayOne) {
         array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
         indexOfSubArrayOne++;
         indexOfMergedArray++;
     }
 
+    // copying the remaining elements of right[]
     while (indexOfSubArrayTwo < subArrayTwo) {
         array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
         indexOfSubArrayTwo++;
@@ -60,11 +77,13 @@ void mergeSort(int array[], int const begin, int const end) {
     merge(array, begin, mid, end);
 }
 
+// Utility Function
 void printArray(int A[], int size) {
     for (auto i = 0; i < size; i++)
         cout << A[i] << " ";
 }
 
+// Driver Code
 int main() {
     int arr[] = {12, 11, 13, 5, 6, 7};
     auto arr_size = sizeof(arr) / sizeof(arr[0]);
