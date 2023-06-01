62f51c83854ecbfd18b0a294393f12739fc0aca4
Jimin Kim <0305jimin@gmail.com>
2023-06-01 22:19:43 +0900

    Refactor: using namespace and trivial modifications

--- C++/Algorithms/N-Queens problem Branch and Bound.cpp
+++ C++/Algorithms/N-Queens problem Branch and Bound.cpp
@@ -19,13 +19,14 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+using namespace std;
 
 struct State {
-    std::vector<int> queens;
+    vector<int> queens;
     int row;
 };
 
-bool isSafe(const std::vector<int>& queens, int row, int col) {
+bool isSafe(const vector<int>& queens, int row, int col) {
     for (int i = 0; i < row; i++) {
         if (queens[i] == col || queens[i] - col == i - row || queens[i] - col == row - i) {
             return false;
@@ -34,9 +35,9 @@ bool isSafe(const std::vector<int>& queens, int row, int col) {
     return true;
 }
 
-std::vector<std::vector<int>> solveNQueens(int n) {
-    std::vector<std::vector<int>> solutions;
-    std::queue<State> states;
+vector<vector<int> > solveNQueens(int n) {
+    vector<vector<int> > solutions;
+    queue<State> states;
 
     State initial_state;
     initial_state.row = 0;
@@ -65,28 +66,28 @@ std::vector<std::vector<int>> solveNQueens(int n) {
     return solutions;
 }
 
-void printSolutions(const std::vector<std::vector<int>>& solutions) {
+void printSolutions(const vector<vector<int> >& solutions) {
     int num_solutions = solutions.size();
     if (num_solutions == 0) {
-        std::cout << "No solutions found.";
+        cout << "No solutions found.";
     } else {
-        std::cout << "Found " << num_solutions << " solution(s):\n";
+        cout << "Found " << num_solutions << " solution(s):\n";
         for (const auto& solution : solutions) {
             for (int row = 0; row < solution.size(); row++) {
                 for (int col = 0; col < solution.size(); col++) {
-                    std::cout << (solution[row] == col ? "Q " : ". ");
+                    cout << (solution[row] == col ? "Q " : ". ");
                 }
-                std::cout << "\n";
+                cout << "\n";
             }
-            std::cout << "\n";
+            cout << "\n";
         }
     }
 }
 
 int main() {
-    int n = 8; // Board size
+    int n = 4; // Board size
 
-    std::vector<std::vector<int>> solutions = solveNQueens(n);
+    vector<vector<int> > solutions = solveNQueens(n);
     printSolutions(solutions);
 
     return 0;
