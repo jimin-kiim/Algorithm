dfd9facdb245857fdeb7b88fa7720039efc61555
Jimin Kim <0305jimin@gmail.com>
2023-06-26 14:24:44 +0900

    Refactor: without using array

--- C++/BOJ/10871.cpp
+++ C++/BOJ/10871.cpp
@@ -14,19 +14,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, x, a[10005]; // goal for PS: not writing a clean code.
-// it's solving the problem as fast as possible in the limited time
 int main () {
-    ios::sync_with_stdio(0); // in fact, the arg type should be bool.
-    // but for PS, it doesn't matter
+    ios::sync_with_stdio(0);
     cin.tie(0);
+    int n, x, t;
     cin >> n >> x;
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    for(int i = 0; i < n; i++) {
-        if (a[i] < x) {
-            cout << a[i] << " ";
+    while(n--) {
+        cin >> t;
+        if(t < x) {
+            cout << t << " ";
         }
     }
 }
