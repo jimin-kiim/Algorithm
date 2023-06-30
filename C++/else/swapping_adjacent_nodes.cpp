779f7176c649179168501f4b2ba47daf3aea6dbd
Jimin Kim <0305jimin@gmail.com>
2023-06-30 18:11:50 +0900

    Feat: node swapping; LinkedList

---
+++ C++/else/swapping_adjacent_nodes.cpp
@@ -0,0 +1,49 @@
+//
+// Created by 김지민 on 2023/06/30.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head != NULL && head -> next != NULL) {
+            ListNode* target = head -> next;
+            if(target -> next != NULL ){
+                ListNode* rb_next_target = target -> next;
+                head-> next = swapPairs(rb_next_target);
+            } else {
+                head -> next = target -> next;
+            }
+            target -> next = head;
+            return target;
+        }
+        return head;
+    }
+};
+
+int main() {
+    ListNode node_one = ListNode(1);
+    ListNode node_two = ListNode(2);
+    ListNode node_three = ListNode(3);
+    ListNode node_four = ListNode(4);
+    node_one.next = &node_two;
+    node_two.next = &node_three;
+    node_three.next = &node_four;
+    Solution sol = Solution();
+    ListNode *node = sol.swapPairs(&node_one);
+
+    while(node) {
+       cout << node -> val << " ";
+       node = node -> next;
+    }
+}
