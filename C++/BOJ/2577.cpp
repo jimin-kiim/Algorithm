4d55ffbcc7ecee3e9e2a2334ab6e0938745bdcd2
Jimin Kim <0305jimin@gmail.com>
2023-07-04 12:59:37 +0900

    Feat: 2577_ using number itself as int

--- C++/BOJ/2577.cpp
+++ C++/BOJ/2577.cpp
@@ -12,15 +12,33 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int a, b, c, d;
-    cin >> a;
-    cin >> b;
-    cin >> c;
+    cin >> a >> b >> c;
     d = a * b * c;
+
+#if false
+    /**
+     * solution 1
+     * - to_string
+     * - ascii
+     */
+
     string result = to_string(d);
 
     for (char c : result) {
         frequency[c-'0'] += 1;
     }
+#else
+    /**
+     * solution 2
+     * using int itself
+     */
+
+    while(d > 0) {
+        frequency[d%10]++;
+        d /= 10;
+    }
+#endif
+
     for (int i = 0; i < 10; i++) {
         cout << frequency[i] << "\n";
     }
