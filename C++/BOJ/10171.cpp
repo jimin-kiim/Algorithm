564d5980e78f70965d310e8d21f9acb978e863e7
Jimin Kim <0305jimin@gmail.com>
2023-07-05 20:02:09 +0900

    Feat: 10171_escape sequence

---
+++ C++/BOJ/10171.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 김지민 on 2023/07/05.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "\\    /\\" << "\n";
+    cout << " )  ( \')" << "\n";
+    cout << "(  /  )" << "\n";
+    cout << " \\(__)|" << "\n";
+}
