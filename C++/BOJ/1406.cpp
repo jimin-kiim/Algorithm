bf856f6dc741021c53ac28dab78248a62621b062
Jimin Kim <0305jimin@gmail.com>
2023-07-05 21:40:21 +0900

    Feat: 1406_iterator

---
+++ C++/BOJ/1406.cpp
@@ -0,0 +1,31 @@
+//
+// Created by 김지민 on 2023/07/05.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string input;
+    cin >> input;
+    list<char> L = {};
+    for (char e: input) {
+        L.push_back(e);
+    }
+    int n;
+    cin >> n;
+    list<char>::iterator t = L.end();
+    while(n--) {
+        char command, data;
+        cin >> command;
+        if (command == 'L' && t != L.begin()) t--;
+        else if (command == 'D' && t != L.end()) t++;
+        else if (command == 'B' && t != L.begin()) t = L.erase(--t);
+        else if (command == 'P'){
+            cin >> data;
+            L.insert(t, data);
+        }
+    }
+    for (auto e: L) {
+        cout << e;
+    }
+}
