5a5eeea988340c161a98aab95b988901c53ea509
Jimin Kim <0305jimin@gmail.com>
2023-07-07 23:02:47 +0900

    Refactor: using vector and its functions
    min, max

--- C++/BOJ/2752.cpp
+++ C++/BOJ/2752.cpp
@@ -5,24 +5,11 @@
 using namespace std;
 
 int main() {
-    int arr[3];
-    for (int i = 0; i < 3; i++) {
-        cin >> arr[i];
-    }
-    int min = 1000000;
-    int min_index = 0;
-    int max = 0;
-    int max_index = 0;
-    for (int i = 0; i < 3; i++) {
-        if(arr[i] < min) {
-            min = arr[i];
-            min_index = i;
-        }
-        if(arr[i] > max) {
-            max = arr[i];
-            max_index = i;
-        }
-    }
-    int middle = 3 - min_index - max_index;
-    cout << arr[min_index] << " " <<  arr[middle] << " " << arr[max_index];
+    int a, b, c;
+    int min_value, max_value;
+    cin >> a >> b >> c;
+    min_value = min({a, b, c});
+    max_value = max({a, b, c});
+    int middle = a + b + c - min_value - max_value;
+    cout << min_value << " " <<  middle << " " << max_value;
 }
