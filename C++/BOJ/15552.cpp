41a41d09499cd7b336bb01e53ccc228d3aee3c64
Jimin Kim <0305jimin@gmail.com>
2023-07-13 01:48:04 +0900

    Feat: 15552_settings needed to finish within a strict time limit

---
+++ C++/BOJ/15552.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 김지민 on 2023/07/13.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << "\n";
+    }
+}
