eaa1f34a942e113380ae127cc6255494d15c3eaf
Jimin Kim <0305jimin@gmail.com>
2023-07-13 22:42:00 +0900

    Feat: 2440

---
+++ C++/BOJ/2440.cpp
@@ -0,0 +1,16 @@
+//
+// Created by 김지민 on 2023/07/13.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while (n--) {
+        for (int i = 0 ; i < n + 1 ; i++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+}
