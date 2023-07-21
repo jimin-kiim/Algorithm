4a227b7b8143e201ef6719c6415094765a27977f
Jimin Kim <0305jimin@gmail.com>
2023-07-21 23:45:22 +0900

    Refactor: 5397(2)

--- C++/BOJ/5397(2).cpp
+++ C++/BOJ/5397(2).cpp
@@ -9,27 +9,17 @@ int main() {
     cin >> n;
 
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            cout << " ";
-        }
-        for (int j = 0; j < (i + 1) * 2 - 1; j++) {
-            cout << "*";
-        }
+        for (int j = 0; j < n - i - 1; j++) cout << " ";
+        for (int j = 0; j < (i + 1) * 2 - 1; j++) cout << "*";
         cout << "\n";
     }
 
-    for (int i = 0; i < 2 * n - 1; i++) {
-        cout << "*";
-    }
+    for (int i = 0; i < 2 * n - 1; i++) cout << "*";
     cout << "\n";
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < i + 1; j++) {
-            cout << " ";
-        }
-        for (int j = 0;  j < (n - i - 1) * 2 - 1; j++) {
-            cout << "*";
-        }
+    for (int i = n - 1; i > 0 ; i--) {
+        for (int j = 0; j < n - i; j++) cout << " ";
+        for (int j = 0;  j < i * 2 - 1; j++) cout << "*";
         cout << "\n";
     }
 }
