329cb760e89534f28c230ce042656a7d68fe01c6
Jimin Kim <0305jimin@gmail.com>
2023-07-31 16:12:14 +0900

    WIP: 2493_ using stack but time out

--- C++/BOJ/2493.cpp
+++ C++/BOJ/2493.cpp
@@ -5,28 +5,33 @@
 using namespace std;
 
 int main() {
-    int n;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, height;
     cin >> n;
-    vector<int> towers;
+    stack<int> towers;
+    int max = 0;
 
     for (int i = 0; i < n; i++) {
-        int height;
         cin >> height;
-        towers.push_back(height);
-        bool found = false;
-        for (int j = i; j >= 0; j--) {
-            if (towers[j] > height) {
-                cout << j + 1 << " ";
-                found = true;
-                break;
+        if (!i) {
+            cout << 0 << " ";
+        } else {
+            cout << max << " ";
+            if (towers.top() < height) {
+                max = i + 1;
             }
         }
-        if (!found) cout << 0 << " ";
+        towers.push(height);
     }
 }
 
 /*
- * 각 탑. 왼쪽 방향으로 차례로 순회. 본인 높이보다 높으면서 가장 먼저 발견되는 탑
+ * 각 탑. 왼쪽 방향으로 차례로 순회. 본인 높이보다 높으면서 가장 먼저 발견되는 탑 찾기
  * -> 시간 초과
- * 
+ *
+ * 바로 직전 높이보다 방금 들어온 높이가 더 크면 max 값을 현재 값으로 변경
+ * max - 1 값. 타워 번호 출력
+ * -> 시간 초과
  */
