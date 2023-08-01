df189374bee1fed13c30e56a6c350336cc5f1ac5
Jimin Kim <0305jimin@gmail.com>
2023-08-01 19:52:59 +0900

    Feat: 6198_stack

--- C++/BOJ/6198.cpp
+++ C++/BOJ/6198.cpp
@@ -12,15 +12,15 @@ int main() {
     cin >> n;
 
     long long count = 0;
-    int buildings[n];
-    for (int i = 0; i < n; i++) {
-         cin >> buildings[i];
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (buildings[i] > buildings[j]) count++;
-            else break;
+    long long height;
+    stack<int> buildings;
+    while (n--) {
+        cin >> height;
+        while (!buildings.empty() && buildings.top() <= height ) {
+            buildings.pop();
         }
+        count += buildings.size();
+        buildings.push(height);
     }
     cout << count;
 }
@@ -28,6 +28,9 @@ int main() {
 /*
  * 본인 이후에 들어오는 값들 중 본인보다 크거나 같은 값이 들어오기 전까지의 개수 카운팅.
  * -> 시간 초과
+ *
+ * 각각의 높이가 들어왔을 때 이를 볼 수 있는 빌딩의 개수를 센다.
+ * 해당 빌딩 보다 높지만 뒤에 들어오는 빌딩은 어차피 이를 볼 수 없기 때문에 신경을 쓰지 않아도 된다.
  *
  *
  */
