e083a49500a66de2cf677bdd81c0b0d4fd05440a
Jimin Kim <0305jimin@gmail.com>
2023-08-25 22:12:28 +0900

    Refactor: 1463_dp(1), min
    removing arbitrary maximum value for comparison

--- C++/BOJ/1463.cpp
+++ C++/BOJ/1463.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 int dp[1000001];
-#define MAX 9999;
 
 int main() {
     ios::sync_with_stdio(0);
@@ -13,20 +12,14 @@ int main() {
 
     int n;
     cin >> n;
-    dp[1] = 0;
     for (int i = 2; i <= n; i++) {
-        int a = MAX;
-        int b = MAX;
-        int c;
+        dp[i] = 1 + dp[i - 1];
         if (i % 3 == 0) {
-            a = 1 + dp[i / 3];
+            dp[i] = min(dp[i], 1 + dp[i / 3]);
         }
         if (i % 2 == 0) {
-            b = 1 + dp[i / 2];
+            dp[i] = min(dp[i], 1 + dp[i / 2]);
         }
-        c = 1 + dp[i - 1];
-        int min_val = min(a, b);
-        dp[i] = min(min_val, c);
     }
     cout << dp[n];
 }
