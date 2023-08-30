fe9e51228bdc632597276f13b12ec7e28e2d711b
Jimin Kim <0305jimin@gmail.com>
2023-08-31 04:26:01 +0900

    Refactor: 9663; verifying `is_safe` by tracking passed nodes

--- C++/BOJ/9663.cpp
+++ C++/BOJ/9663.cpp
@@ -6,13 +6,11 @@ using namespace std;
 
 int n;
 int ans;
-bool unavailable[16];
 int q[16];
 
 bool is_safe(int col, int row) {
-    if (unavailable[col]) return 0;
     for (int i = 0; i < row; i++) {
-        if (col - q[i] == row - i || q[i] - col == row - i) return 0;
+        if (q[i] == col || col - q[i] == row - i || q[i] - col == row - i) return 0;
     }
     return 1;
 }
@@ -25,10 +23,8 @@ void func(int row) {
 
     for (int col = 0; col < n; col++) {
         if (is_safe(col, row)) {
-            unavailable[col] = 1;
             q[row] = col;
             func(row + 1);
-            unavailable[col] = 0;
         }
     }
 }
