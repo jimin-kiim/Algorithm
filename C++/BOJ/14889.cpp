c4ca022895f9cdc01734a176ee21656c75086263
Jimin Kim <0305jimin@gmail.com>
2023-09-04 19:23:12 +0900

    Feat: 14889_fill, do-while, next_permutation, min, abs

--- C++/BOJ/14889.cpp
+++ C++/BOJ/14889.cpp
@@ -9,37 +9,6 @@ int n;
 int team[21];
 int arr[21][21];
 
-void func(int cur, int start_size) {
-    int start = 0;
-    int link = 0;
-    if (start_size == n / 2) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) continue;
-                if (team[i] == team[j] && team[i] == 1) {
-                    start += arr[i - 1][j - 1];
-                } else if (team[i] == team[j] && team[i] == 0) {
-                    link += arr[i - 1][j - 1];
-                }
-            }
-        }
-        int dif = start - link;
-        if (dif < 0 ) {
-            if (-dif < min_val) min_val = -dif;
-        } else {
-            if (dif < min_val) min_val = dif;
-        }
-        return;
-    }
-    if (cur == n) return;
-    for (int i = cur; i <= n; i++) {
-        func(cur + 1,  start_size);
-        team[i] = 1;
-        func(cur + 1, start_size + 1);
-        team[i] = 0;
-    }
-}
-
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -50,6 +19,20 @@ int main() {
             cin >> arr[i][j];
         }
     }
-    func(0, 0);
+    fill(team + n / 2, team + n, 1);
+    do {
+        int dif = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (team[i] != team[j]) continue;
+                if (team[i] == 1) {
+                    dif += arr[i][j] + arr[j][i];
+                } else {
+                    dif -= arr[i][j] + arr[j][i];
+                }
+            }
+        }
+        min_val = min(min_val, abs(dif));
+    } while (next_permutation(team, team + n));
     cout << min_val;
 }
