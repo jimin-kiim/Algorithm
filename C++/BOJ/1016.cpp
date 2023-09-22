fda8be94b5ac479784cb6920bc6e0f79d1b2b3b9
Jimin Kim <0305jimin@gmail.com>
2023-09-22 19:42:51 +0900

    WIP: 1016_ timeout

--- C++/BOJ/1016.cpp
+++ C++/BOJ/1016.cpp
@@ -4,25 +4,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define MAX 1000000000000
-int  palette[MAX];
+#define MAX 1000001
+int palette[MAX];
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    for (int i = 2; i < 1000000; i++) {
-        int exp = pow(i, 2);
-        for (int j = 1; exp * j <= MAX; j++) {
-            palette[exp * j] = 1;
+    long long mi, ma;
+    cin >> mi >> ma;
+
+    for (int i = 2; i <= 1000000; i++) {
+        long long exp = pow(i, 2);
+        for (int j = 1; exp * j <= ma; j++) {
+            if (exp * j >= mi) palette[exp * j - mi] = 1;
         }
     }
 
-    int mi, ma;
-    cin >> mi >> ma;
-
     int count = 0;
-    for (int i = mi; i <= ma; i++) {
-        if (palette[i] == 0) count++;
+    for (long long i = mi; i <= ma; i++) {
+        if (palette[i - mi] == 0) count++;
     }
+    cout << count;
 }
+
+// 4, 9, 25, 49, 121, 169, 17^2, 19^2, 23^2, .... 소수의 제곱
