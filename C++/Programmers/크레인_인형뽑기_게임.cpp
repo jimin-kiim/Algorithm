59f61352bf4e5a954185ed5c8e458c76fd544b41
Jimin Kim <0305jimin@gmail.com>
2023-09-26 14:42:49 +0900

    Fix: 크레인 인형뽑기 게임

--- C++/Programmers/크레인_인형뽑기_게임.cpp
+++ C++/Programmers/크레인_인형뽑기_게임.cpp
@@ -10,7 +10,7 @@ int solution(vector<vector<int>> board, vector<int> moves) {
 
     for (int m = 0; m < moves.size(); m++) {
         for (int i = 0; i < board[0].size(); i++) {
-            int doll = board[moves[m]][i];
+            int doll = board[i][moves[m] - 1];
             if (doll != 0) {
                 if (dolls.empty()) {
                     dolls.push(doll);
@@ -20,10 +20,29 @@ int solution(vector<vector<int>> board, vector<int> moves) {
                     dolls.pop();
                     count += 2;
                 }
-                board[moves[m]][i] = 0;
+                board[i][moves[m] - 1] = 0;
                 break;
             }
         }
     }
     return count;
 }
+
+int main() {
+    vector<vector<int>> board = {
+            {0,0,0,0,0},
+            {0,0,1,0,3},
+            {0,2,5,0,1},
+            {4,2,4,4,2},
+            {3,5,1,3,1}};
+    vector<int> moves = {1,5,3,5,1,2,1,4};
+    cout << solution(board, moves);
+}
+
+//4
+//3
+//2
+//1
+//1
+//3
+//4
