bc99d16916a7c5264c765bfb7978ec0f75e46e94
Jimin Kim <0305jimin@gmail.com>
2024-01-18 19:50:07 +0900

    Refactor: 2446_remove duplicates, simplifying codes

--- C++/BOJ/2446.cpp
+++ C++/BOJ/2446.cpp
@@ -12,26 +12,20 @@ int main() {
     cin >> n;
 
     // n - 1 줄
-    for (int i = n - 1; i > 0; i--) {
-        for (int j = 0; j < (n - 1 - i); j++) cout << " "; // 0 <= j < n - 2
-        for (int j = i; j > 0; j--) cout << "*";
-        cout << "*";
-        for (int j = i; j > 0; j--) cout << "*";
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < i; j++) cout << " "; // 0 <= j < n - 2
+        for (int j = 2 * (n - i) - 1; j > 0; j--) cout << "*";
         cout << "\n";
     }
 
     // 한 줄
     for (int i = 0; i < n - 1; i++) cout << " ";
-    cout << "*";
-    for (int i = 0; i < n - 1; i++) cout << " ";
-    cout << "\n";
+    cout << "*\n";
 
     // n - 1 줄
     for (int i = n - 1; i > 0; i--) {
         for (int j = 0; j < i - 1; j++) cout << " "; //  0 <= j < n - 2
-        for (int j = 0; j < (n - i); j++) cout << "*";
-        cout << "*";
-        for (int j = 0; j < (n - i); j++) cout << "*";
+        for (int j = 0; j < 2 * (n - i) + 1; j++) cout << "*";
         cout << "\n";
     }
 }
