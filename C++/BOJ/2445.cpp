92a30c0258e294edb5b23825f825963939ad1c5c
Jimin Kim <0305jimin@gmail.com>
2024-01-18 18:33:21 +0900

    Refactor: 2445

--- C++/BOJ/2445.cpp
+++ C++/BOJ/2445.cpp
@@ -12,36 +12,26 @@ int main() {
     cin >> n;
 
     // n - 1 줄
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < i + 1; j++) { // i + 1 만큼
-            cout << "*";
-        }
-        for (int j = 0; j < (n - i - 1) * 2; j++) {
-            cout << " ";
-        }
-        for (int j = 0; j < i + 1; j++) {
-            cout << "*";
-        }
+    for (int i = 1; i <= n - 1; i++) {
+        for (int j = 1; j <= i; j++) cout << "*";
+        for (int j = 1; j <= (n - i) * 2; j++) cout << " ";
+        for (int j = 1; j <= i; j++) cout << "*";
         cout << "\n";
     }
 
     // 한 줄
-    for (int i = 0; i < n * 2; i++) {
-        cout << "*";
-    }
+    for (int i = 0; i < n * 2; i++) cout << "*";
     cout << "\n";
 
     // n - 1 줄
-    for (int i = n - 1; i > 0; i--) {
-        for (int j = 0; j < i; j++) {
-            cout << "*";
-        }
-        for (int j = 0; j < (n - i) * 2; j++) {
-            cout << " ";
-        }
-        for (int j = 0; j < i; j++) {
-            cout << "*";
-        }
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) cout << "*";
+        for (int j = 1; j <= (n - i) * 2; j++) cout << " ";
+        for (int j = 1; j <= i; j++) cout << "*";
         cout << "\n";
     }
 }
+
+/*
+ * i = 0에서 시작하는 게 별 거 아닌 데도 갑자기 헷갈릴 땐 그냥 눈에 보이는 대로 직관적이게 1부터 하고 <= 쓰기..
+ */
