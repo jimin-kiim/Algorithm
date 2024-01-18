41308da93e82bd351c85b0cf4946a443f21499a7
Jimin Kim <0305jimin@gmail.com>
2024-01-18 18:47:09 +0900

    Feat: 2446

---
+++ C++/BOJ/2446.cpp
@@ -0,0 +1,37 @@
+//
+// Created by 김지민 on 1/18/24.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    // n - 1 줄
+    for (int i = n - 1; i > 0; i--) {
+        for (int j = 0; j < (n - 1 - i); j++) cout << " "; // 0 <= j < n - 2
+        for (int j = i; j > 0; j--) cout << "*";
+        cout << "*";
+        for (int j = i; j > 0; j--) cout << "*";
+        cout << "\n";
+    }
+
+    // 한 줄
+    for (int i = 0; i < n - 1; i++) cout << " ";
+    cout << "*";
+    for (int i = 0; i < n - 1; i++) cout << " ";
+    cout << "\n";
+
+    // n - 1 줄
+    for (int i = n - 1; i > 0; i--) {
+        for (int j = 0; j < i - 1; j++) cout << " "; //  0 <= j < n - 2
+        for (int j = 0; j < (n - i); j++) cout << "*";
+        cout << "*";
+        for (int j = 0; j < (n - i); j++) cout << "*";
+        cout << "\n";
+    }
+}
