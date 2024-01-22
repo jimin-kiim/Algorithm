aaed45042cd979c8c1185c591ba3024a06d00c99
Jimin Kim <0305jimin@gmail.com>
2024-01-22 17:29:51 +0900

    Fix: 2870_ OOB error handling big integer string

--- C++/BOJ/2870.cpp
+++ C++/BOJ/2870.cpp
@@ -4,6 +4,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string erase_zeros(string input) {
+    while(input.size() > 1) {
+        if (input.front() == '0') {
+            input.erase(0, 1);
+        } else {
+            return input;
+        }
+    }
+    return input;
+}
+
+bool cmp(string a,string b) {
+    if (a.length() == b.length()) return a < b;
+    return a.length() < b.length();
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -11,7 +27,7 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> v;
+    vector<string> v;
 
     while (n--) {
         string input;
@@ -23,7 +39,8 @@ int main() {
         for (int i = 0; i < input.size(); i++) {
             if (!is_prev_digit && isalpha(input[i])) continue;
             if (isalpha(input[i])) {
-                v.push_back(stoi(number));
+                number = erase_zeros(number);
+                v.push_back(number);
                 number = "";
                 is_prev_digit = false;
                 continue;
@@ -32,12 +49,13 @@ int main() {
             is_prev_digit = true;
 
             if (i == input.size() - 1) {
-                v.push_back(stoi(number));
+                number = erase_zeros(number);
+                v.push_back(number);
             }
         }
     }
 
-    sort(v.begin(), v.end());
+    sort(v.begin(), v.end(), cmp);
     for (int i = 0; i < v.size(); i++) {
         cout << v.at(i);
         if (i != v.size() - 1) cout << "\n";
