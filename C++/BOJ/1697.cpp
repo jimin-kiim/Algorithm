cf33d1b48ead0d64205f3b5d105f36d5cef8f2ee
Jimin Kim <0305jimin@gmail.com>
2024-01-31 15:18:51 +0900

    Feat: 1697_bfs, t tracking

---
+++ C++/BOJ/1697.cpp
@@ -0,0 +1,70 @@
+//
+// Created by 김지민 on 1/31/24.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+
+//int board[100002];
+int t[100002];
+
+//int dir[3] = {};
+int n, k;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    fill(t, t + 100002, -1);
+
+    cin >> n >> k;
+    queue<int> q;
+    q.push(n);
+    t[n] = 0;
+
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+
+        int nx = x - 1;
+        if (t[nx] >= 0) continue;
+
+        q.push(nx);
+        t[nx] = t[x] + 1;
+        if (nx == k) break;
+
+        nx = x + 1;
+        if (t[nx] >= 0) continue;
+
+        q.push(nx);
+        t[nx] = t[x] + 1;
+        if (nx == k) break;
+
+        nx = x * 2;
+        if (t[nx] >= 0) continue;
+
+        q.push(nx);
+        t[nx] = t[x] + 1;
+        if (nx == k) break;
+    }
+    cout << t[k];
+    cout << "\n";
+    for (int i = 0; i < 20; i++)
+        cout << i << " ";
+    cout << "\n";
+    for (int i = 0; i < 20; i++)
+        cout << t[i] << " ";
+}
+/*
+ * 5 10 20 19 18 17
+ * 5 10 9 18 17
+ *
+ * 일단 순간이동을 시도해서 n이 k 넘어가면 순간 이동 멈추고 1칸씩 줄이기
+ * -> 5 10 20 19 18 17
+ * -> 최소 시간 벗어남
+ *
+ * 미로탐색 문제처럼 각 지점에서의 최소 시간 트래킹해 나가다가
+ * k 도달하면 종료
+ */
