472d5287435c7ebd80e80a8e602a30d739e48366
Jimin Kim <0305jimin@gmail.com>
2024-02-03 23:52:53 +0900

    Feat: 2606_bfs

---
+++ C++/BOJ/2606.cpp
@@ -0,0 +1,42 @@
+//
+// Created by 김지민 on 2/3/24.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    vector<int> arr[101];
+    int vis[101];
+    int num;
+
+    cin >> n >> num;
+    for (int i = 0; i < num; i++) {
+        int u, v;
+        cin >> u >> v;
+        arr[u].push_back(v);
+        arr[v].push_back(u);
+    }
+
+    queue<int> q;
+    q.push(1);
+    vis[1] = 1;
+
+    int count = 0;
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        for (int next: arr[cur]) {
+            if (vis[next] == 1) continue;
+            count++;
+            vis[next] = 1;
+        }
+    }
+
+    cout << count;
+}
