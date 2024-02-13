dd8d0cd33cbb3bcea7509ce2175dfc07b9aa5b5a
Jimin Kim <0305jimin@gmail.com>
2024-02-13 16:37:59 +0900

    Feat: 6558_sieve of eratosathenes

---
+++ C++/BOJ/6558.cpp
@@ -0,0 +1,41 @@
+//
+// Created by 김지민 on 2/13/24.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+#define limit 1000001
+int prime[limit];
+
+void validate_prime_numbers() {
+    for (int i = 2; i < limit; i++) {
+        if (prime[i] == 0) continue; // 이미 소수가 아닌 걸로 판별한 수면 패스
+        for (int j = 2 * i; j < limit; j += i) { // 해당 수에 대한 배수들 invalid 표시하기
+            prime[j] = 0;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    fill(prime + 1, prime + limit, 1);
+    validate_prime_numbers();
+
+    while (true) {
+        int input;
+        cin >> input;
+        if (input == 0) break;
+        int a = 0, b = 0;
+        for (int i = 3; i < input; i++) {
+            if (prime[i] && prime[input - i]) {
+                a = i;
+                b = input - i;
+                break;
+            }
+        }
+        if (a == 0) cout << "Goldbach's conjecture is wrong.\n";
+        else cout << input << " = " << a << " + " << b << "\n";
+    }
+}
