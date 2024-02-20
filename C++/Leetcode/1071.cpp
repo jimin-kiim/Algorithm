4c408ee59072f5d5e77c0a9350ae3440c052acb2
Jimin Kim <0305jimin@gmail.com>
2024-02-20 19:58:47 +0900

    Fix: 1071_ excluding the case when getting gcd string is impossible
    and using gcd function

--- C++/Leetcode/1071.cpp
+++ C++/Leetcode/1071.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
         string result = "";
-        int size = min(str1.size(), str2.size());
+        if (str1 + str2 != str2 + str1) return result; 
+        
+        int size = gcd(str1.size(), str2.size());
 
         for (int i = 0; i < size; i++) {
             if (str1[i] != str2[i]) break;
