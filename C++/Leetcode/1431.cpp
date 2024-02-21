cf1a84188de31e612fc1f5183e538ca8ae65019b
Jimin Kim <0305jimin@gmail.com>
2024-02-21 21:57:32 +0900

    Feat: 1431_max_element

---
+++ C++/Leetcode/1431.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max_value = *max_element(candies.begin(), candies.end());
+        vector<bool> result;
+        for (int i = 0; i < candies.size(); i++) {
+            if (candies[i] + extraCandies >= max_value) {
+                result.push_back(true);
+            } else {
+                result.push_back(false);
+            }
+        }
+
+        return result;
+    }
+};
