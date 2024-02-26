02eb7a74ff4b2d6d44aca208a39ff1b03676ee71
Jimin Kim <0305jimin@gmail.com>
2024-02-26 19:03:15 +0900

    Feat: 15651_backtracking; printing all the possible permutations

---
+++ C++/BOJ/15651.cpp
@@ -0,0 +1,33 @@
+//
+// Created by 김지민 on 2/26/24.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int res[8];
+int n, m;
+
+void func(int k) {
+    if (k == m) {
+        for (int i = 0; i < m; i++) {
+            cout << res[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    int start = 1;
+    if (k != 0) start = res[k - 1] + 1;
+    for (int i = start; i <= n; i++) {
+        res[k] = i;
+        func(k + 1);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    func(0);
+}
