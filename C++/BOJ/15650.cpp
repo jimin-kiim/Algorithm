8a36440051a170e78ffbc1c85be5d7b71e43785f
Jimin Kim <0305jimin@gmail.com>
2024-02-26 18:16:20 +0900

    Feat: 15650_ back tracking basic

---
+++ C++/BOJ/15650.cpp
@@ -0,0 +1,38 @@
+//
+// Created by 김지민 on 2/15/24.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int res[9]; // 정답 수열 기록
+int is_used[9]; // 선택 가능한 수 팔레트
+int n, m;
+
+void func(int k) { // k: 현재까지 택한 수의 개수
+    if (k == m) { // 종료 조건: m 개를 모두 택한 경우
+        for (int i = 0; i < m; i++) // res에 기록해 둔 수 차례로 출력
+            cout << res[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    int start = 1; // 첫 시작이면 k = 0: start = 1
+    if (k != 0) start = res[k - 1] + 1; // 첫 시작은 아닐 때 k != 0: st = arr[k - 1] + 1
+    for (int i = start; i <= n; i++) {
+        if(!is_used[i]) {
+            res[k] = i; // 아직 쓰인 적 없으면 k번째 수를 i로
+            is_used[i] = 1; // i 사용했다고 표시
+
+            func(k + 1); // k + 1번째 다음 수 정하러 한 단계 더 들어감
+            is_used[i] = 0; // k번째 수를 i로 정한 모든 경우에 대해 다 확인했으니 i를 이제 사용하지 않았다고 표시
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    func(0);
+}
