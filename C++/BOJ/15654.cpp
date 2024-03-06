92206ee940a31d6163697395ea48533dba46c26a
Jimin Kim <0305jimin@gmail.com>
2024-03-06 23:33:04 +0900

    Feat: 15654_backtracking printing all the possible permutations
    consist of ascending order of digits while duplication is not allowed in a single permutation

---
+++ C++/BOJ/15654.cpp
@@ -0,0 +1,45 @@
+//
+// Created by 김지민 on 3/6/24.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int res[8];
+int palette[8];
+int is_used[8];
+
+int n, m;
+
+void func(int k) {
+    if (k == m) {
+        for (int i = 0; i < m; i++) {
+            cout << res[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (is_used[i] != 1) {
+            res[k] = palette[i];
+            is_used[i] = 1;
+            func(k + 1);
+            is_used[i] = 0;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> palette[i];
+    }
+
+    sort(palette, palette + n);
+    func(0);
+}
