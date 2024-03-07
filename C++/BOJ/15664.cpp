25605dbaa40490685e50c6606e9f5b0fd77756d1
Jimin Kim <0305jimin@gmail.com>
2024-03-08 02:37:03 +0900

    Feat: 15664_backtracking printing unique combinations in an ascending
    order while the palette contains duplicated numbers

---
+++ C++/BOJ/15664.cpp
@@ -0,0 +1,50 @@
+//
+// Created by 김지민 on 3/8/24.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int res_indices[8];
+int palette[8];
+int is_used[8];
+
+void func(int k) {
+    if (k == m) {
+        for (int i = 0; i < m; i++) {
+            cout << palette[res_indices[i]] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    int start = 0;
+    int prev = 0; // consequential permutations should be unique
+    if (k != 0) start = res_indices[k - 1]; // should not be aligned in a descending order
+    for (int i = start; i < n; i++) {
+        if (is_used[i] != 1 && prev != palette[i]) {
+            res_indices[k] = i;
+            prev = palette[i];
+            is_used[i] = 1;
+            func(k + 1);
+            is_used[i] = 0;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> palette[i];
+    }
+
+    sort(palette, palette + n);
+    func(0);
+}
+/*
+ * combination(permutation but the additional condition says that the numbers in a single permutation should not be aligned in a descending order so it ends up to be combination, at last)
+ *
+ */
