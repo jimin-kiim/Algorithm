d15eadbbd63159819448024f2e61ab74ccb1a697
Jimin Kim <0305jimin@gmail.com>
2024-03-09 23:11:11 +0900

    Refactor: 15650_setting the starting point for traversal by retrieving
    the value from the function's parameter instead of fetching the previously stored value in res

--- C++/BOJ/15650.cpp
+++ C++/BOJ/15650.cpp
@@ -8,7 +8,7 @@ int res[9]; // 정답 수열 기록
 int is_used[9]; // 선택 가능한 수 팔레트
 int n, m;
 
-void func(int k) { // k: 현재까지 택한 수의 개수
+void func(int start, int k) { // k: 현재까지 택한 수의 개수
     if (k == m) { // 종료 조건: m 개를 모두 택한 경우
         for (int i = 0; i < m; i++) // res에 기록해 둔 수 차례로 출력
             cout << res[i] << ' ';
@@ -16,14 +16,11 @@ void func(int k) { // k: 현재까지 택한 수의 개수
         return;
     }
 
-    int start = 1; // 첫 시작이면 k = 0: start = 1
-    if (k != 0) start = res[k - 1] + 1; // 첫 시작은 아닐 때 k != 0: st = arr[k - 1] + 1
     for (int i = start; i <= n; i++) {
         if(!is_used[i]) {
             res[k] = i; // 아직 쓰인 적 없으면 k번째 수를 i로
             is_used[i] = 1; // i 사용했다고 표시
-
-            func(k + 1); // k + 1번째 다음 수 정하러 한 단계 더 들어감
+            func(i + 1, k + 1); // k + 1번째 다음 수 정하러 한 단계 더 들어감. 순회 시작은 현재 값 + 1부터 (수열 구성 시 사용 후보 숫자는 중복 불가이자 오름차순 정렬로 구성)
             is_used[i] = 0; // k번째 수를 i로 정한 모든 경우에 대해 다 확인했으니 i를 이제 사용하지 않았다고 표시
         }
     }
@@ -34,5 +31,5 @@ int main() {
     cin.tie(0);
 
     cin >> n >> m;
-    func(0);
+    func(1, 0);
 }
