143bff3825c37e21fe1b4331439f586ef2528170
Jimin Kim <0305jimin@gmail.com>
2024-03-10 21:55:09 +0900

    Feat: 15894_implicit type casting, maximum range of int

---
+++ C++/BOJ/15894.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 김지민 on 3/10/24.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    cout << 4 * n;
+    // int * int => int. if long long is expected, n should be initialized as long long
+}
