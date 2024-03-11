2e4eba264ca5040020bfc2a631a5930f9cac61fb
Jimin Kim <0305jimin@gmail.com>
2024-03-11 23:26:33 +0900

    Feat: 5567_traversing a graph using bfs counting the number of nodes
    that are at most two steps away from the root node

---
+++ C++/BOJ/5567.cpp
@@ -0,0 +1,58 @@
+//
+// Created by 김지민 on 3/10/24.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> friends[501];
+int vis[501];
+int tot_count;
+
+void bfs();
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int f1, f2;
+        cin >> f1 >> f2;
+        friends[f1].push_back(f2);
+        friends[f2].push_back(f1);
+    }
+
+    bfs();
+    cout << tot_count;
+}
+
+void bfs() {
+    queue<int> q;
+    q.push(1);
+    vis[1] = 1;
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        int flag = 0;
+        for (int closest: friends[1]) {
+            if (closest == cur) flag = 1;
+        }
+        if (!flag && cur != 1) continue;
+
+        for (int f: friends[cur]) {
+            if (vis[f] == 1) continue;
+
+            tot_count++;
+            vis[f] = 1;
+
+            q.push(f);
+        }
+    }
+}
+
+/*
+ * traversing graph
+ * but if the distance is more than 2, it doesn't increase the count
+ * traversing bfs only in two depth
+ */
