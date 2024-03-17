57961692d8b92ca608f48f1858c3dd66be4cc6b9
Jimin Kim <0305jimin@gmail.com>
2024-03-17 15:35:04 +0900

    Feat: 2193_dp
    resulting a string whose length is n while 1s are not placed consecutively and doesn't start with 0

---
+++ C++/BOJ/2193.cpp
@@ -0,0 +1,39 @@
+//
+// Created by 김지민 on 3/17/24.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+long long dp[91]; // storing the number of all the possible cases. index == n
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    dp[1] = 1;
+    dp[2] = 1;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    cout << dp[n];
+}
+
+/*
+ * dp[1]: 1 -> 1
+ * dp[2]: 10 -> 1
+ * dp[3]: 101: 10 + 1, 100: 10 + 0 -> 2
+ * dp[4]: 1010,        1001,  1000 -> 3
+ * dp[5]: 10101, 10100,10010, 10001, 10000 -> 5
+ * dp[6]: 101010,101001,101000, 100101, 100100, 100010, 100001, 100000 -> 8
+ * (when the previous string ends with 1, concatenating 0 is the only case
+ * when the previous string ends with 0, concatenating 0 or 1 these two cases exist)
+ *
+ * if the n is odd, dp[n - 1]
+ * if the n is even, ...
+ *
+ *
+ *
+ */
