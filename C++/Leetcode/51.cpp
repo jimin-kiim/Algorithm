9b9c69998178555b7e0a8e16bda455a2d38bc561
Jimin Kim <0305jimin@gmail.com>
2025-05-16 00:26:24 +0900

    Leetcode-51: modifying wrong parts

--- C++/Leetcode/51.cpp
+++ C++/Leetcode/51.cpp
@@ -4,30 +4,38 @@
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-        int chess_board[n][n];
-        for (int i = 0; i < n; i++) {
-            chess_board[i].fill(0);
-        }
+        vector<vector<char>> chess_board(n, vector<char> (n, '.'));
+        vector<vector<string>> result;
 
         for (int i = 0; i < n; i++) {
+            string row;
             for (int j = 0; j < n; j++) {
                 // is there no Q in same column (i is same and j is different.)
-                for (int c = j - 1; j - 1 >= 0; j--) {
-                    if (chess_board[i][c] == 'Q' ) break;
-                }
-
-                // is there no Q in the same diagonal line. ()
-                for (int r = i - 1; r >= 0; r--) {
-                    for (int c = j - 1; c>=0; c--) {
-                        if (chess_board[r][c] == 'Q') break;
+                for (int c = j - 1; c >= 0; c--) {
+                    if (chess_board[i][c] == 'Q' ) {
+                        row += '.';
+                        break;
                     }
                 }
 
+                // is there no Q in the same diagonal line. ()
+                for (int r = i - 1, c = j - 1; r >= 0 && c >=0; c--, r--) {
+                    if (chess_board[r][c] == 'Q') {
+                        row += '.';
+                        break;
+                    }
+                }
                 chess_board[i][j] = 'Q';
+                row += 'Q';
+            }
+            for (int j = 0; j < n; j++) {
+                result[i].push_back(row);
             }
         }
+        return result;
     }
 };
+
 
 /*
  * N-Queens Problem
