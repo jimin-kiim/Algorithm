9f70f454582a68cf28da4a6a20286480dc216cf5
Jimin Kim <0305jimin@gmail.com>
2025-05-17 21:19:09 +0900

    Leetcode-51: using backtracking model with multiple methods

--- C++/Leetcode/51.cpp
+++ C++/Leetcode/51.cpp
@@ -3,47 +3,47 @@
 //
 class Solution {
 public:
+        bool is_valid(vector<vector<char>> chess_board, int row, int col, int n) {
+            // is there no Q in same column (i is same and j is different.)
+            for (int r = row - 1; r >= 0; r--) {
+                if (chess_board[r][col] == 'Q' ) return false;
+            }
+
+            // is there no Q in the same diagonal line. (left)
+            for (int r = row - 1, c = col - 1; r >= 0 && c >= 0; c--, r--) {
+                if (chess_board[r][c] == 'Q') return false;
+            }
+
+            // is there no Q in the same diagonal line. (right)
+            for (int r = row - 1, c = col + 1; r >= 0 && c < n; r--, c++) {
+                if (chess_board[r][c] == 'Q') return false;
+            }
+
+            return true;
+    }
+
+    void recurrence(vector<vector<char>> chess_board, int row, int n, vector<vector<char>> & result) {
+        string result_row;
+        for (int col = 0; col < n; col++) {
+            if (is_valid(chess_board, row, col, n)) {
+                chess_board[row][col] = 'Q';
+                result_row += 'Q';
+               recurrence(chess_board, row + 1, n, result);
+            }
+            chess_board[row][col] = '.';
+            result_row += '.';
+        }
+        result.push_back(result_row);
+    }
+
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<char>> chess_board(n, vector<char> (n, '.'));
         vector<vector<string>> result;
 
-        for (int i = 0; i < n; i++) {
-            string row;
-            for (int j = 0; j < n; j++) {
-                // is there no Q in same column (i is same and j is different.)
-                for (int c = j - 1; c >= 0; c--) {
-                    if (chess_board[i][c] == 'Q' ) {
-                        chess_board[i][j] = '.';
-                        row += '.';
-                        break;
-                    }
-                }
-
-                // is there no Q in the same diagonal line. (left)
-                for (int r = i - 1, c = j - 1; r >= 0 && c >= 0; c--, r--) {
-                    if (chess_board[r][c] == 'Q') {
-                        chess_board[i][j] = '.';
-                        row += '.';
-                        break;
-                    }
-                }
-
-                // is there no Q in the same diagonal line. (right)
-                for (int r = i - 1, c = j + 1; r >= 0 && c < n; r--, c++) {
-                    if (chess_board[r][c] == 'Q') {
-                        chess_board[i][j] = '.';
-                        row += '.';
-                        break;
-                    }
-                }
-                chess_board[i][j] = 'Q';
-                row += 'Q';
-            }
-            for (int j = 0; j < n; j++) {
-                result[i].push_back(row);
-            }
+        for (int row = 0; row < n; row++) {
+            recurrence(chess_board, row, n, result);
         }
-        return result;
+        result.push_back()
     }
 };
 
