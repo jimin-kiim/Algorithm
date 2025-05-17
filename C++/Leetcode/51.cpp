ca55d8f6ac9ab351bb7a232d33f3368d944f55ed
Jimin Kim <0305jimin@gmail.com>
2025-05-17 22:22:24 +0900

    Leetcode-51: adding comment

--- C++/Leetcode/51.cpp
+++ C++/Leetcode/51.cpp
@@ -1,6 +1,15 @@
 //
 // Created by 김지민 on 5/15/25.
 //
+
+/*
+ * placing queens ~= building n permutations, each has three '.' and one 'Q'.
+ * permutation => backtracking
+ * clear promising function => backtracking
+ *
+ * backtracking => recurrence function.
+ * recurrence function => termination condition +  promising function + building the solution + recurrence + backtracking)
+ */
 class Solution {
 public:
         bool is_valid(vector<vector<char>> chess_board, int row, int col, int n) {
@@ -23,12 +32,10 @@ public:
         }
 
     void recurrence(vector<vector<char>> chess_board, int row, int n, vector<vector<string>> & result) {
+            // termination condition
         if (row >= n) {
             vector<string> a_solution;
-//            for (int i = 0; i < n; i++) {
-//                string row_as_a_string(chess_board[i].begin(), chess_board[i].end()));
-//                a_solution.push_back(row_as_a_string);
-//            }
+
             for (const auto& each_row: chess_board) {
                 a_solution.push_back(string(each_row.begin(), each_row.end())); // each row: vector<char>. convert the value into a single stirng.
             }
@@ -37,17 +44,24 @@ public:
         }
 
         for (int col = 0; col < n; col++) {
-            if (is_valid(chess_board, row, col, n)) {
+            // promising function
+            if (is_valid(chess_board, row, col, n)) { // too long so extracted as a separated function
+                // building a solution
                 chess_board[row][col] = 'Q';
+                // recurrence
                recurrence(chess_board, row + 1, n, result);
             }
+            // backtracking
             chess_board[row][col] = '.';
         }
     }
 
     vector<vector<string>> solveNQueens(int n) {
+            // initializing
         vector<vector<char>> chess_board(n, vector<char> (n, '.'));
         vector<vector<string>> result;
+
+        // start solving the problem
         recurrence(chess_board, 0, n, result);
 
         return result;
