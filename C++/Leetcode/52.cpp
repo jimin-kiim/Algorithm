2af41e9a8db9873b75638ba49d9c2a6f0c987de8
Jimin Kim <0305jimin@gmail.com>
2025-05-22 01:14:21 +0900

    Leetcode-52: resolving error

--- C++/Leetcode/52.cpp
+++ C++/Leetcode/52.cpp
@@ -15,40 +15,59 @@ class Solution {
 public:
     int totalNQueens(int n) {
         vector<vector<char> > chess_board(n, vector<char>(n, '.'));
-        int result;
-        for (int i = 0; i < n; i++) {
-            result += recurrence(chess_board, 0, n, 0);
-        }
-
+        int result = 0;
+        recurrence(chess_board, 0, n, result);
+        return result;
     }
 
 private:
-    bool isSafe(vector<vector<char> > &chess_board, int row, int col, int n) {
-        for (int r = row; r >= 0; r--) {
+    bool isValid(vector<vector<char> > &chess_board, int row, int col, int n) {
+        // verify same column
+        for (int r = row - 1; r >= 0; r--) {
             if (chess_board[r][col] == 'Q') return false;
         }
 
-        for (int r = row, c = col; r >= 0, c >= 0; r--, c--) {
+        // verify same diagonal(left)
+        for (int r = row - 1, c = col - 1; r >=0 && c >= 0; r--, c--) {
             if(chess_board[r][c] == 'Q') return false;
         }
 
-        for (int r = row, c = col; r >= 0, c < n; r--, c++) {
+        // verify same diagonal(right)
+        for (int r = row - 1, c = col + 1; r >= 0 && c < n; r--, c++) {
             if(chess_board[r][c] == 'Q') return false;
         }
-
         return true;
     }
 
-    int recurrence(vector<vector<char> > &chess_board, int row, int n, int result) {
+    void recurrence(vector<vector<char> > &chess_board, int row, int n, int& result) {
+        // termination
         if (row >= n) {
-            return result + 1;
-        }
-        for (int col = 0; col < n; col++) {
-            if (isSafe(chess_board, row, col, n)) {
-                chess_board[row][col] = 'Q';
-                recurrence(chess_board, row + 1, n, result);
-                chess_board[row][col] = '.';
+            result += 1;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    cout << chess_board[i][j] << " ";
+                }
+                cout << endl;
             }
+            cout << endl;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            // promising function
+            if (isValid(chess_board, row, col, n)) {
+                // visiting node and building the solution
+                chess_board[row][col] = 'Q';
+                // DFS
+                recurrence(chess_board, row + 1, n, result);
+            }
+            // backtracking
+            chess_board[row][col] = '.';
         }
     }
 };
+
+int main() {
+    Solution solution;
+    solution.totalNQueens(4);
+}
