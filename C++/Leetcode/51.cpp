92de810e4edf758e555483b24b091dc5bdf4611d
Jimin Kim <0305jimin@gmail.com>
2025-05-22 01:15:18 +0900

    Leetcode-51: testing with main function

--- C++/Leetcode/51.cpp
+++ C++/Leetcode/51.cpp
@@ -10,9 +10,13 @@
  * backtracking => recurrence function.
  * recurrence function => termination condition +  promising function + building the solution + recurrence + backtracking)
  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-        bool is_valid(vector<vector<char>> chess_board, int row, int col, int n) {
+        bool is_valid(vector<vector<char> > chess_board, int row, int col, int n) {
             // is there no Q in same column (i is same and j is different.)
             for (int r = row - 1; r >= 0; r--) {
                 if (chess_board[r][col] == 'Q' ) return false;
@@ -31,14 +35,16 @@ public:
             return true;
         }
 
-    void recurrence(vector<vector<char>> chess_board, int row, int n, vector<vector<string>> & result) {
+    void recurrence(vector<vector<char> > chess_board, int row, int n, vector<vector<string> > & result) {
             // termination condition
         if (row >= n) {
             vector<string> a_solution;
 
             for (const auto& each_row: chess_board) {
                 a_solution.push_back(string(each_row.begin(), each_row.end())); // each row: vector<char>. convert the value into a single stirng.
+                cout << string(each_row.begin(), each_row.end()) << endl;
             }
+            cout << endl;
             result.push_back(a_solution);
             return;
         }
@@ -56,10 +62,10 @@ public:
         }
     }
 
-    vector<vector<string>> solveNQueens(int n) {
-            // initializing
-        vector<vector<char>> chess_board(n, vector<char> (n, '.'));
-        vector<vector<string>> result;
+    vector<vector<string> > solveNQueens(int n) {
+        // initializing
+        vector<vector<char> > chess_board(n, vector<char> (n, '.'));
+        vector<vector<string> > result;
 
         // start solving the problem
         recurrence(chess_board, 0, n, result);
@@ -68,6 +74,10 @@ public:
     }
 };
 
+int main() {
+    Solution solution;
+    solution.solveNQueens(4);
+}
 
 /*
  * N-Queens Problem
