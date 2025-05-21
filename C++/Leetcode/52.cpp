3221eacbba0f23561e13e42a297e3f319771b8d9
Jimin Kim <0305jimin@gmail.com>
2025-05-21 23:47:22 +0900

    Leetcode-52: adding comments before solving the problem

---
+++ C++/Leetcode/52.cpp
@@ -0,0 +1,19 @@
+//
+// Created by 김지민 on 5/21/25.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+// N Queens problem 1. BF - too many possible cases
+//                  => 2. Backtracking(pruning with promising function)
+//                  => recurrence
+//                      => termination condition + visiting node + promising function + recurrence + backtracking
+// promising function:
+//              - two queens cannot be placed in a same row/column and diagonal line
+
+class Solution {
+public:
+    int totalNQueens(int n) {
+
+    }
+};
