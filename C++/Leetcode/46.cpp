a10ce8eec335b3224d8591a42201212f0c617e61
Jimin Kim <0305jimin@gmail.com>
2025-05-30 21:40:01 +0900

    Leetcode-46: resolve overflow error

--- C++/Leetcode/46.cpp
+++ C++/Leetcode/46.cpp
@@ -10,21 +10,21 @@ public:
         vector<vector<int> > result;
         vector<int> permutation;
         vector<int> is_used(nums.size(), 0);
-        recurrence(0, nums, permutation, is_used, result);
+        recurrence(nums, permutation, is_used, result);
         return result;
     }
 
-    void recurrence(int k, vector<int>& nums, vector<int> permutation, vector<int>& is_used, vector<vector<int>>& result) { // k means kth number in a permutation
-        if (k == nums.size()) {
+    void recurrence(vector<int>& nums, vector<int> permutation, vector<int>& is_used, vector<vector<int>>& result) { // k means kth number in a permutation
+        if (permutation.size() == nums.size()) {
             result.push_back(permutation);
             return;
         }
         for (int i = 0; i < nums.size(); i++) {
-            if(!is_used[nums[i]]) {
+            if(!is_used[i]) {
                 permutation.push_back(nums[i]);
-                is_used[nums[i]] = 1;
-                recurrence(k + 1, nums, permutation, is_used, result);
-                is_used[nums[i]] = 0;
+                is_used[i] = 1;
+                recurrence(nums, permutation, is_used, result);
+                is_used[i] = 0;
                 permutation.pop_back();
             }
         }
