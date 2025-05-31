23688b8b2a4611d3576385ffa78a1134db3acee3
Jimin Kim <0305jimin@gmail.com>
2025-05-31 14:59:36 +0900

    BOJ-15651: creating permuations allowing the duplicate usage of the elements for it.

--- C++/BOJ/15651.cpp
+++ C++/BOJ/15651.cpp
@@ -4,21 +4,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int res[8];
+int permutation[8];
 int n, m;
 
-void func(int k) {
-    if (k == m) {
+void func(int k) { // meaning: the sequence is filled out upto kth index.
+    // termination condition
+    if (k == m) { // when a sequence is built upto mth index, when it reached the leaf node, exit the function.
         for (int i = 0; i < m; i++) {
-            cout << res[i] << " ";
+            cout << permutation[i] << " "; // printing out the built permutation.
         }
         cout << "\n";
         return;
     }
 
     for (int i = 1; i <= n; i++) {
-        res[k] = i;
-        func(k + 1);
+        // building a solution
+        permutation[k] = i; // set the number i as the kth number of the sequence.
+        // recurrence
+        func(k + 1); // build the item of the sequence, setting (k+1)th index of the it.
     }
 }
 
@@ -26,8 +29,8 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    cin >> n >> m;
-    func(0);
+    cin >> n >> m; // n: the number of the items that would be used to build a sequence, m: the length of the sequence to be made up.
+    func(0); // it means that the sequence is filled out upto 0 index.
 }
 
 /*
