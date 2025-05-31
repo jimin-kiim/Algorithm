6135bac13e389ab8cebc3f596d5c60ee79f3b50f
Jimin Kim <0305jimin@gmail.com>
2025-05-31 15:06:38 +0900

    BOJ-15656: 15651 using mapping method with an array

--- C++/BOJ/15656.cpp
+++ C++/BOJ/15656.cpp
@@ -8,17 +8,21 @@ int n, m;
 int res_indices[8];
 int palette[8];
 
+// overall structure is similar to the no.15651 but the numbers traversed in for-statement are the indices, not the actual values that would be used for creating the permutation.
 void func(int k) {
-    if (k == m) {
+    // termination condition
+    if (k == m) { // when the mth element is set, when it reached the leaf node, exit the function.
         for (int i = 0; i < m; i++) {
-            cout << palette[res_indices[i]] << " ";
+            cout << palette[res_indices[i]] << " "; // printing out the corresponding elements by mapping the index value to the actual value using the palette array.
         }
         cout << "\n";
         return;
     }
 
     for (int i = 0; i < n; i++) {
-        res_indices[k] = i;
+        // building a solution
+        res_indices[k] = i; // record that ith element in the palette would be used as a kth element of the sequence.
+        // recurrence
         func(k + 1);
     }
 }
@@ -29,10 +33,10 @@ int main() {
 
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
-        cin >> palette[i];
+        cin >> palette[i]; // the elements that would be used to build a sequence is given.
     }
 
-    sort(palette, palette + n);
+    sort(palette, palette + n); // sort the elements of the palette. in order to print the sequences in ascending order.
     func(0);
 }
 /*
