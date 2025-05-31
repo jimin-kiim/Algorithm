dabcf8b75487867577afbf73bc733e384ae40157
Jimin Kim <0305jimin@gmail.com>
2025-05-31 18:19:27 +0900

    Leetcode-17: creating combination with multiple palette

--- C++/Leetcode/17.cpp
+++ C++/Leetcode/17.cpp
@@ -7,60 +7,44 @@ using namespace std;
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        /*
-         *         vector<vector<char> > tablet = {{'a', 'b', 'c'},
-                                        {'d', 'e', 'f'},
-                                        {'g', 'h', 'i'},
-                                        {'j', 'k', 'l'},
-                                        {'m', 'n', 'o'},
-                                        {'p', 'q', 'r', 's'},
-                                        {'t', 'u', 'v'},
-                                        {'w', 'x', 'y', 'z'}
-        };
-         */
-
-        vector<string> tablet = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> tablet = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}; // multiple palettes
 
         vector<int> digits_int;
-        for (char digit: digits_int) {
+        for (char digit: digits) {
             digits_int.push_back(digit - '0');
         }
 
-        vector<string> result;
+        vector<string> result = {};
         string sequence = "";
 
+        // start calling the recurrence function
         combine(0, result, sequence, digits_int, tablet);
         return result;
     }
 
-    void combine(int start, vector<string>& result, string sequence, vector<int>&digits_int, vector<string> tablet) {
+    void combine(int key, vector<string>& result, string sequence, vector<int>&digits_int, vector<string> tablet) {
+        // termination condition
+        if (digits_int.size() == 0) {
+            return;
+        }
+
         if (sequence.size() == digits_int.size()) {
             result.push_back(sequence);
             return;
         }
 
-        /*
-        sequence.push_back(tablet[digits_int[i]+2]);
-        sequence.push_back(tablet[digits_init[j]+2]);
-        sequence.push_back(tablet[digits_init[k]+2]);
-        sequence.push_back(tablet[digits_init[l]+2]);
-        */
-
-        /*
-        for (int i = start; i < digits_int.size(); i++) {
-            sequence.push_back(tablet[digits_int[i] + 2]);
-            combine(i + 1, result, sequence, digits_int, tablet);
-            sequence.pop_back(tablet[digits_int[i] + 2])
+        // building a combination
+        for (int j = 0; j < tablet[digits_int[key] - 2].size(); j++) {
+            sequence.push_back(tablet[digits_int[key] - 2][j]);
+            // recurrence
+            combine(key + 1, result, sequence, digits_int, tablet);
+            // backtracking
+            sequence.pop_back();
         }
-        */
-
-        for (int i = start; i < digits_int.size(); i++) {
-            for (int j = 0; j < [digits_int[i] + 2].size(); j++) {
-                sequence.push_back(tablet[digits_int[i] + 2][j]);
-                combine(i + 1, result, sequence, digits_int, tablet);
-                sequence.pop_back(tablet[digits_int[i] + 2][j]);
-            }
-        }
-
     }
 };
+
+int main() {
+    Solution solution;
+    solution.letterCombinations("");
+}
