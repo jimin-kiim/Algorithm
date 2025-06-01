ea4767018da7ef4d55180c0cbfbcf7eba11316d0
Jimin Kim <0305jimin@gmail.com>
2025-06-01 21:17:43 +0900

    BOJ-15650: adding comments

--- C++/BOJ/1012_bfs.cpp
+++ C++/BOJ/1012_bfs.cpp
@@ -29,55 +29,73 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    // the number of test cases.
     int t;
     cin >> t;
+
+    // resolve the problem; t times.
     for (int i = 0; i < t; i++) {
         initialize_boards();
         count_worm = 0;
 
         int k;
+        // get the width and height of the field (board) & the number of the cabbages.
         cin >> n >> m >> k;
 
+        // get the coordination values of the positions of the k cabbages.
         int x, y;
         for (int j = 0; j < k; j++) {
             cin >> x >> y;
             board[x][y] = 1;
         }
 
+        // count how many worms are needed to keep the cabbage field healthy.
         count_worms();
+
+        // print out the number of worms needed to keep the field healthy.
         cout << count_worm;
+
+        // if it's not the last test case, print out a line break
         if (i != t - 1) cout << "\n";
     }
 }
 
 void count_worms() {
-    queue<pair<int, int> > q;
+    queue<pair<int, int> > q; // BFS
 
     // 배추 영역 시작점 찾기
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            // 배추 없는 곳이나 이미 방문한 위치면 패스
+            // if it's not a position where the cabbage is placed or already visited, pass and keep going finding out the cabbage.
             if (board[i][j] == 0 || vis[i][j]) continue;
 
             // 처음 도착한 배추 영역
+            // when a cabbage is found, update the number of worm needed and mark the position visited.
             count_worm++;
             vis[i][j] = 1;
-            q.push({i, j});
+            q.push({i, j}); // store the current position in the queue
 
             // 한 마리의 배추 지렁이가 보호할 영역 돌기. 방문한 영역 표시
+            // to mark the area verified as a range of a visited cabbage group.
             while (!q.empty()) {
                 pair<int, int> cur = q.front();
                 q.pop();
 
+                // verifying how wide the cabbage field is and visit as a verified one.
                 for (int dir = 0; dir < 4; dir++) {
                     int nx = cur.X + dx[dir];
                     int ny = cur.Y + dy[dir];
 
+                    // check if it's outside the field or already visited or not a cabbage.
                     if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
                     if (vis[nx][ny] || board[nx][ny] == 0) continue;
 
+                    // mark that it's visited
                     vis[nx][ny] = 1;
+                    // update the latest position visited.
                     q.push({nx, ny});
+
+
                 }
             }
         }
--- C++/BOJ/15650.cpp
+++ C++/BOJ/15650.cpp
@@ -9,6 +9,7 @@ int is_used[9]; // 선택 가능한 수 팔레트
 int n, m;
 
 void func(int start, int k) { // k: 현재까지 택한 수의 개수
+    // termination condition
     if (k == m) { // 종료 조건: m 개를 모두 택한 경우
         for (int i = 0; i < m; i++) // res에 기록해 둔 수 차례로 출력
             cout << res[i] << ' ';
@@ -16,11 +17,16 @@ void func(int start, int k) { // k: 현재까지 택한 수의 개수
         return;
     }
 
+    // creating a new sequence
     for (int i = start; i <= n; i++) {
+        // promising function - not allowing the duplicate usage of an element
         if(!is_used[i]) {
+            // building a solution - storing as a sequence constitution and marking as used.
             res[k] = i; // 아직 쓰인 적 없으면 k번째 수를 i로
             is_used[i] = 1; // i 사용했다고 표시
+            // recurrence - keep going creating a sequence
             func(i + 1, k + 1); // k + 1번째 다음 수 정하러 한 단계 더 들어감. 순회 시작은 현재 값 + 1부터 (수열 구성 시 사용 후보 숫자는 중복 불가이자 오름차순 정렬로 구성)
+            // backtracking - marking as unused.
             is_used[i] = 0; // k번째 수를 i로 정한 모든 경우에 대해 다 확인했으니 i를 이제 사용하지 않았다고 표시
         }
     }
